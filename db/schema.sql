53a8f1c8ab0d4d83e0e7ff95a36b42a07aa87dca
PeterMazzoli <Peter.mazzoli@gmail.com>
2017-03-23 19:46:50 -0400

    clean up code

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,4 @@
--- First and Last Name
--- Address
--- Email Address
--- Zip Code
--- Photo Link
-
 CREATE DATABASE blogger4;
 
 USE blogger4;
 
-
--- CREATE DATABASE IF NOT EXISTS welcomeHome_db;
--- USE welcomeHome_db;
-
--- # If the table already exists, remove it before trying to create the table again
--- DROP TABLE IF EXISTS users;
-
--- # Create the burgers table
--- CREATE TABLE users (
--- id int NOT NULL AUTO_INCREMENT,
--- firstLast_name varchar(100) NOT NULL,
--- address varchar(250) NOT NULL,
--- email varchar(250) NOT NULL,
--- zip_code INTEGER(7) NOT NULL,
--- photo varchar(250),
--- PRIMARY KEY (id)
--- );
